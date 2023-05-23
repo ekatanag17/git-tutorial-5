291069d35531b5111abfec69a01469e9ce6cfca9
Ekata Nagrale <ekatanag17@gmail.com>
2023-05-23 23:48:54 +0530

    my commit

---
+++ dom.js
@@ -0,0 +1,13 @@
+var items = document.getElementsByClassName('list-group-item1');
+
+items[0].style.background = ' pink';
+
+
+
+
+var items = document.getElementsByTagName("li");
+document.getElementById("items").innerHTML = items['1'].innerHTML;
+
+
+
+
